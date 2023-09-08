019bc6aad5576a04962ff064a65df8b9002664dd
Renata <110370964+An-Renata@users.noreply.github.com>
2023-09-08 12:32:49 +0300

    media queries

--- static/js/markup.js
+++ static/js/markup.js
@@ -2,13 +2,11 @@
 const renderSearchResults = (book) => {
   return `
   <li class="search-result">
-    <div>
       ${
         book.book_cover
           ? `<img class="search-book-cover" src="${book.book_cover}" alt="book cover photo" />`
           : `<div class=no-image-box">No image</div>`
       }
-    </div>
     <div class="book-info-searches">
       <h3 class="book-title">${book.title}
       </h3>
@@ -21,17 +19,17 @@ const renderSearchResults = (book) => {
       </p>
       <button class="show-more-btn" data-book-key=${book.key}>Show more</button>
     </div>
-    <div class="btn-main-book" >
-      <button class="add-btn add-want-to-read transition " data-book-key=${
-        book.key
-      }>
-        Want to read
-      </button>
-      <button class="add-btn add-currently-reading transition " data-book-key=${
-        book.key
-      }>
-        Currently reading
-      </button>
+    <div class="btn-main-book mobile-query" >
+        <button class="add-btn add-want-to-read transition " data-book-key=${
+          book.key
+        }>
+          Want to read
+        </button>
+        <button class="add-btn add-currently-reading transition " data-book-key=${
+          book.key
+        }>
+          Currently reading
+        </button>
       <button class="add-btn add-to-read transition " data-book-key=${
         book.key
       }>Finished</button>
